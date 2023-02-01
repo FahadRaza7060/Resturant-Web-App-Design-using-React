f6580a5168f2968cc8cf5bd903d5bf8bc930bef1
FahadRaza7060 <fahadraza7060@gmail.com>
2023-02-01 15:02:20 +0500

    First Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Resturant from "./Component/Basics/Resturant";
+
+// import UseState from './Component/Hooks/useState';
+
+const App = () => {
+    return <Resturant />;
+    
+ 
+};
+
+export default App;
